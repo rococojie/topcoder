4d09b48e44fda31418a85c6bec2b8837edfbcdb0
rococojie <mantianwu@hotmail.com>
2012-06-21 09:34:37 +0800

    third

--- topcoder-410.cpp
+++ topcoder-410.cpp
@@ -28,17 +28,17 @@ int getBasePoint(int newPoint, int start, int end){
 	else  return newPoint;
 }
 
-void setNewStart(int &start, int basePoint, int k) {
-	if(basePoint == start) start = basePoint;
-	else start = basePoint + k - 1;
+int setNewPoint(int oldPoint, int basePoint, int k) {
+
+	int newPoint = 0;
+
+	if(basePoint == oldPoint) newPoint = basePoint;
+	else newPoint = basePoint + k - 1;
+	
+	return newPoint;
 
 }
 
-void setNewEnd(int &end, int basePoint, int k) {
-	if(basePoint == end) end = basePoint;
-	else end = basePoint + k - 1;
-
-}
 int ContiguousCacheEasy::bytesRead(int n, int k, vector <int> addresses) {
 
 	int startPoint = 0;
@@ -52,8 +52,8 @@ int ContiguousCacheEasy::bytesRead(int n, int k, vector <int> addresses) {
 		bytes = abs(addresses[i] - basePoint);
 		tatolbytes += getByte(k, bytes);
 		
-		setNewStart(start, bytes, k);
-		setNewEnd( end, base, k);
+		start = setNewPoint(start, bytes, k);
+		end = setNewPoint( end, bytes, k);
 		
 	}
 
