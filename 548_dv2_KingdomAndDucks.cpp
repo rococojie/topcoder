4963dfd9af9f7a4fb6eaa914bee7d28ef406d673
rococojie <mantianwu@hotmail.com>
2012-12-25 11:20:31 +0800

    the  problem of 548_dv2 on topcoder

---
+++ 548_dv2_KingdomAndDucks.cpp
@@ -0,0 +1,189 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class KingdomAndDucks {
+public:
+	int minDucks(vector <int>);
+};
+
+int KingdomAndDucks::minDucks(vector <int> duckTypes) {
+	int maxNum = 0;
+	map<int, int> ducks;
+
+	for(int i = 0; i< duckTypes.size(); i++) {
+		ducks[duckTypes[i]]++;
+
+		if (ducks[duckTypes[i]] > maxNum)  maxNum = ducks[duckTypes[i]];
+	}
+	
+
+	return ducks.size() * maxNum;
+
+}
+
+
+double test0() {
+	int t0[] = {5, 8};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	KingdomAndDucks * obj = new KingdomAndDucks();
+	clock_t start = clock();
+	int my_answer = obj->minDucks(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 2;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int t0[] = {4, 7, 4, 7, 4};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	KingdomAndDucks * obj = new KingdomAndDucks();
+	clock_t start = clock();
+	int my_answer = obj->minDucks(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 6;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int t0[] = {17, 17, 19, 23, 23, 19, 19, 17, 17};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	KingdomAndDucks * obj = new KingdomAndDucks();
+	clock_t start = clock();
+	int my_answer = obj->minDucks(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 12;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int t0[] = {50};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	KingdomAndDucks * obj = new KingdomAndDucks();
+	clock_t start = clock();
+	int my_answer = obj->minDucks(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int t0[] = {10, 10, 10, 10, 10};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	KingdomAndDucks * obj = new KingdomAndDucks();
+	clock_t start = clock();
+	int my_answer = obj->minDucks(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 5;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
