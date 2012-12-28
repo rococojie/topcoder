1afcb8a6f486c0614f2977e3cc4eaa5b658ac2b7
rococojie <mantianwu@hotmail.com>
2012-12-28 10:26:10 +0800

    the problem of 546_dv2 on topcoder

---
+++ 546_dv2_ContestWinner.cpp
@@ -0,0 +1,163 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class ContestWinner {
+public:
+	int getWinner(vector <int>);
+};
+
+int ContestWinner::getWinner(vector <int> events) {
+	map<int ,int > imap;
+	int result = 0;
+	int times = 0;
+
+	for(int i =0; i<events.size(); i++) {
+		imap[events[i]]++;
+		if(imap[events[i]] >times) {
+			times++;
+			result = events[i];
+		}
+	}
+
+	return result;
+}
+
+
+double test0() {
+	int t0[] = {4,7,4,1};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	ContestWinner * obj = new ContestWinner();
+	clock_t start = clock();
+	int my_answer = obj->getWinner(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int t0[] = {10,20,30,40,50};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	ContestWinner * obj = new ContestWinner();
+	clock_t start = clock();
+	int my_answer = obj->getWinner(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 10;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int t0[] = {123,123,456,456,456,123};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	ContestWinner * obj = new ContestWinner();
+	clock_t start = clock();
+	int my_answer = obj->getWinner(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 456;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int t0[] = {1,2,2,3,3,3,4,4,4,4};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	ContestWinner * obj = new ContestWinner();
+	clock_t start = clock();
+	int my_answer = obj->getWinner(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
