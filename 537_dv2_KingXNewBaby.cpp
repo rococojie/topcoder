fdf981f92a9c5dea6ca6a9fad97830b0fd98f032
rococojie <mantianwu@hotmail.com>
2013-01-07 13:46:03 +0800

    the problem of 537 on topcoder

---
+++ 537_dv2_KingXNewBaby.cpp
@@ -0,0 +1,241 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class KingXNewBaby {
+public:
+	string isValid(string);
+};
+
+string KingXNewBaby::isValid(string name) {
+
+	string result = "YES";
+	string vowels("aeiuo"); 
+	map<char,int> words;
+	int len = name.size();
+
+	if(name.size() != 8) return "NO";
+	char n ;
+	for(int i =0; i<len; i++) {
+		if(vowels.find(name[i], 0) != string::npos ) {
+			words[name[i]]++;
+			n = name[i];
+		}	
+	}
+
+	if(words.size()!= 1 || words[n] != 2) result = "NO";
+	return result;
+}
+
+
+double test0() {
+	string p0 = "dengklek";
+	KingXNewBaby * obj = new KingXNewBaby();
+	clock_t start = clock();
+	string my_answer = obj->isValid(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "YES";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "gofushar";
+	KingXNewBaby * obj = new KingXNewBaby();
+	clock_t start = clock();
+	string my_answer = obj->isValid(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "NO";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "dolphinigle";
+	KingXNewBaby * obj = new KingXNewBaby();
+	clock_t start = clock();
+	string my_answer = obj->isValid(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "NO";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "mystictc";
+	KingXNewBaby * obj = new KingXNewBaby();
+	clock_t start = clock();
+	string my_answer = obj->isValid(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "NO";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string p0 = "rngringo";
+	KingXNewBaby * obj = new KingXNewBaby();
+	clock_t start = clock();
+	string my_answer = obj->isValid(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "NO";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test5() {
+	string p0 = "misof";
+	KingXNewBaby * obj = new KingXNewBaby();
+	clock_t start = clock();
+	string my_answer = obj->isValid(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "NO";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test6() {
+	string p0 = "metelsky";
+	KingXNewBaby * obj = new KingXNewBaby();
+	clock_t start = clock();
+	string my_answer = obj->isValid(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "YES";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	time = test5();
+	if (time < 0)
+		errors = true;
+	
+	time = test6();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
