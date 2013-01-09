bd77c0483c83d0d8db7df9e5c7318da7a4de8624
rococojie <mantianwu@hotmail.com>
2013-01-09 10:32:56 +0800

    the problem of 528 on topcoder

---
+++ 528_dv2_MinCostPalindrome.cpp
@@ -0,0 +1,250 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class MinCostPalindrome {
+public:
+	int getMinimum(string, int, int);
+};
+
+int MinCostPalindrome::getMinimum(string s, int oCost, int xCost) {
+	
+	int result = 0;
+	for(int i=0, j=s.size()-1; i<j; i++, j--) {
+		if(s[i] != s[j] ) {
+			if(s[i] !='?' && s[j] != '?') {  result = -1;	break; }
+			else if((s[i] == '?' && s[j] == 'o') || (s[i] == 'o' && s[j] == '?')) result += oCost;
+			else if((s[i] == '?' && s[j] == 'x') || (s[i] == 'x' && s[j] == '?')) result += xCost;
+		}else if(s[i] == s[j] && s[i] == '?') result += min(oCost, xCost)*2;
+	}
+	
+	return result;
+	
+}
+
+
+double test0() {
+	string p0 = "oxo?xox?";
+	int p1 = 3;
+	int p2 = 5;
+	MinCostPalindrome * obj = new MinCostPalindrome();
+	clock_t start = clock();
+	int my_answer = obj->getMinimum(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 8;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "x??x";
+	int p1 = 9;
+	int p2 = 4;
+	MinCostPalindrome * obj = new MinCostPalindrome();
+	clock_t start = clock();
+	int my_answer = obj->getMinimum(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 8;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "ooooxxxx";
+	int p1 = 12;
+	int p2 = 34;
+	MinCostPalindrome * obj = new MinCostPalindrome();
+	clock_t start = clock();
+	int my_answer = obj->getMinimum(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = -1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "oxoxooxxxxooxoxo";
+	int p1 = 7;
+	int p2 = 4;
+	MinCostPalindrome * obj = new MinCostPalindrome();
+	clock_t start = clock();
+	int my_answer = obj->getMinimum(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string p0 = "?o";
+	int p1 = 6;
+	int p2 = 2;
+	MinCostPalindrome * obj = new MinCostPalindrome();
+	clock_t start = clock();
+	int my_answer = obj->getMinimum(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 6;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test5() {
+	string p0 = "????????????????????";
+	int p1 = 50;
+	int p2 = 49;
+	MinCostPalindrome * obj = new MinCostPalindrome();
+	clock_t start = clock();
+	int my_answer = obj->getMinimum(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 980;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test6() {
+	string p0 = "o??oxxo?xoox?ox??x??" ;
+	int p1 = 3;
+	int p2 = 10;
+	MinCostPalindrome * obj = new MinCostPalindrome();
+	clock_t start = clock();
+	int my_answer = obj->getMinimum(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 38;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	time = test5();
+	if (time < 0)
+		errors = true;
+	
+	time = test6();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
