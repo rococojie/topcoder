55813d0aa95586fd91f1c69a7532623032381272
rococojie <mantianwu@hotmail.com>
2013-01-27 11:12:20 +0800

    the problem of 519 on topcoder

---
+++ 519_dv2_WhichDay.cpp
@@ -0,0 +1,161 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class WhichDay {
+public:
+	string getDay(vector <string>);
+};
+
+string WhichDay::getDay(vector <string> notOnThisDay) {
+	string Days[7] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+	string meetingDay = "";
+	
+	for(int i=0; i<7; i++) {
+		if(notOnThisDay.end() == find(notOnThisDay.begin(), notOnThisDay.end(), Days[i])) {
+			meetingDay = Days[i];
+			break;
+		}
+	}
+	
+	return meetingDay;
+}
+
+
+double test0() {
+	string t0[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	WhichDay * obj = new WhichDay();
+	clock_t start = clock();
+	string my_answer = obj->getDay(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "Saturday";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string t0[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Friday", "Thursday"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	WhichDay * obj = new WhichDay();
+	clock_t start = clock();
+	string my_answer = obj->getDay(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "Saturday";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string t0[] = {"Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	WhichDay * obj = new WhichDay();
+	clock_t start = clock();
+	string my_answer = obj->getDay(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "Wednesday";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string t0[] = {"Sunday", "Friday", "Tuesday", "Wednesday", "Monday", "Saturday"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	WhichDay * obj = new WhichDay();
+	clock_t start = clock();
+	string my_answer = obj->getDay(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "Thursday";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
