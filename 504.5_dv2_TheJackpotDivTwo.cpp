1765f88957986ccd205516c85dc8150009574fb4
rococojie <mantianwu@hotmail.com>
2013-02-13 15:09:06 +0800

    the problem of 504.5 on topcoder

---
+++ 504.5_dv2_TheJackpotDivTwo.cpp
@@ -0,0 +1,231 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class TheJackpotDivTwo {
+public:
+	vector <int> find(vector <int>, int);
+};
+
+vector <int> TheJackpotDivTwo::find(vector <int> money, int jackpot) {
+	vector<int> result(money);
+	sort(result.begin(), result.end()) ;
+	
+	for(int i=0;i<jackpot; i++) {
+		result[0]++;
+		sort(result.begin(),result.end());
+	}
+	
+	return result;
+}
+
+
+double test0() {
+	int t0[] = {1, 2, 3, 4};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int p1 = 2;
+	TheJackpotDivTwo * obj = new TheJackpotDivTwo();
+	clock_t start = clock();
+	vector <int> my_answer = obj->find(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = {2, 3, 3, 4 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int t0[] = {4, 7};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int p1 = 1;
+	TheJackpotDivTwo * obj = new TheJackpotDivTwo();
+	clock_t start = clock();
+	vector <int> my_answer = obj->find(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = {5, 7 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int t0[] = {1};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int p1 = 100;
+	TheJackpotDivTwo * obj = new TheJackpotDivTwo();
+	clock_t start = clock();
+	vector <int> my_answer = obj->find(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = {101 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int t0[] = {21, 85, 6, 54, 70, 100, 91, 60, 71};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int p1 = 15;
+	TheJackpotDivTwo * obj = new TheJackpotDivTwo();
+	clock_t start = clock();
+	vector <int> my_answer = obj->find(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = {21, 21, 54, 60, 70, 71, 85, 91, 100 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
