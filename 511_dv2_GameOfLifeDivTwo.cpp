d541af621b60bf64e494c394651078d18417798b
rococojie <mantianwu@hotmail.com>
2013-02-13 13:38:20 +0800

    the problem of 511 on topcoder

---
+++ 511_dv2_GameOfLifeDivTwo.cpp
@@ -0,0 +1,226 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class GameOfLifeDivTwo {
+public:
+	string theSimulation(string, int);
+};
+
+bool isLive(string init, int i) {
+	int num  = 0;
+	int len = init.size();
+	
+	num += (init[i] == '1') + (init[(i-1 +len)%len] == '1') +(init[(i+1+len)%len] == '1') ;
+	
+	if(num >=2) return true;
+	else return false;
+}
+string GameOfLifeDivTwo::theSimulation(string init, int T) {
+	string result = init;
+	string temp = init;
+
+	for(int i=0; i<T; i++) {
+		string temp = "";
+		for(int j=0; j<init.size(); j++) {
+			if(isLive(result, j))  temp += '1';
+			else temp += '0';
+		}
+		result = temp;
+	}
+	
+	return result;
+}
+
+
+double test0() {
+	string p0 = "01010";
+	int p1 = 2;
+	GameOfLifeDivTwo * obj = new GameOfLifeDivTwo();
+	clock_t start = clock();
+	string my_answer = obj->theSimulation(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "00000";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "010101";
+	int p1 = 5;
+	GameOfLifeDivTwo * obj = new GameOfLifeDivTwo();
+	clock_t start = clock();
+	string my_answer = obj->theSimulation(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "101010";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "111010";
+	int p1 = 58;
+	GameOfLifeDivTwo * obj = new GameOfLifeDivTwo();
+	clock_t start = clock();
+	string my_answer = obj->theSimulation(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "111111";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "111111111";
+	int p1 = 511;
+	GameOfLifeDivTwo * obj = new GameOfLifeDivTwo();
+	clock_t start = clock();
+	string my_answer = obj->theSimulation(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "111111111";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string p0 = "110010000010111110010100001001";
+	int p1 = 1000;
+	GameOfLifeDivTwo * obj = new GameOfLifeDivTwo();
+	clock_t start = clock();
+	string my_answer = obj->theSimulation(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "110000000001111110000000000001";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test5() {
+	string p0 = "00101110011";
+	int p1 = 0;
+	GameOfLifeDivTwo * obj = new GameOfLifeDivTwo();
+	clock_t start = clock();
+	string my_answer = obj->theSimulation(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "00101110011";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	time = test5();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
