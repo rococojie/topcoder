5c5520008697cf1055727a5da7a9241978a911ac
rococojie <mantianwu@hotmail.com>
2013-02-13 13:38:09 +0800

    the problem of 510 on topcoder

---
+++ 510_dv2_TheAlmostLuckyNumbersDivTwo.cpp
@@ -0,0 +1,173 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class TheAlmostLuckyNumbersDivTwo {
+public:
+	int find(int, int);
+};
+bool isAlmostLuckyNumber(int i) {
+	int temp = i;
+	int times = 0;
+	bool result = true;
+	while(temp >0) {
+		if(temp%10 != 4 && temp %10 != 7){
+			times++;
+			if(times > 1) {
+				result = false;
+				break;
+			}
+		}
+		temp /= 10;
+	}
+	
+	return result;
+}
+int TheAlmostLuckyNumbersDivTwo::find(int a, int b) {
+	int result = 0;
+	
+	for(int i=a; i<=b; i++) {
+		if(isAlmostLuckyNumber(i)) result++;
+	}
+	
+	return result;
+}
+
+
+double test0() {
+	int p0 = 4;
+	int p1 = 7;
+	TheAlmostLuckyNumbersDivTwo * obj = new TheAlmostLuckyNumbersDivTwo();
+	clock_t start = clock();
+	int my_answer = obj->find(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 8;
+	int p1 = 19;
+	TheAlmostLuckyNumbersDivTwo * obj = new TheAlmostLuckyNumbersDivTwo();
+	clock_t start = clock();
+	int my_answer = obj->find(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 28;
+	int p1 = 33;
+	TheAlmostLuckyNumbersDivTwo * obj = new TheAlmostLuckyNumbersDivTwo();
+	clock_t start = clock();
+	int my_answer = obj->find(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 1234;
+	int p1 = 4321;
+	TheAlmostLuckyNumbersDivTwo * obj = new TheAlmostLuckyNumbersDivTwo();
+	clock_t start = clock();
+	int my_answer = obj->find(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 36;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
