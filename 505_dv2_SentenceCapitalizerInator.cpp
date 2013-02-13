b86318fe6fa896ce356e587e2737200e97b8c9ae
rococojie <mantianwu@hotmail.com>
2013-02-13 15:09:20 +0800

    the problem of 505 on topcoder

---
+++ 505_dv2_SentenceCapitalizerInator.cpp
@@ -0,0 +1,159 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class SentenceCapitalizerInator {
+public:
+	string fixCaps(string);
+};
+
+string SentenceCapitalizerInator::fixCaps(string paragraph) {
+	
+	string result(paragraph);
+	
+	result[0] -= 32;
+	for(int i=1; i<result.size(); ) {
+		if('.' == result[i]&& (i != result.size()-1 || ' ' == result[i+1])) {
+			i = i+2;
+			result[i] -= 32;
+		}else i++;
+	}
+
+	return result;
+	
+}
+
+
+double test0() {
+	string p0 = "hello programmer. welcome to topcoder.";
+	SentenceCapitalizerInator * obj = new SentenceCapitalizerInator();
+	clock_t start = clock();
+	string my_answer = obj->fixCaps(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "Hello programmer. Welcome to topcoder.";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "one.";
+	SentenceCapitalizerInator * obj = new SentenceCapitalizerInator();
+	clock_t start = clock();
+	string my_answer = obj->fixCaps(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "One.";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "not really. english. qwertyuio. a. xyz.";
+	SentenceCapitalizerInator * obj = new SentenceCapitalizerInator();
+	clock_t start = clock();
+	string my_answer = obj->fixCaps(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "Not really. English. Qwertyuio. A. Xyz.";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "example four. the long fourth example. a. b. c. d.";
+	SentenceCapitalizerInator * obj = new SentenceCapitalizerInator();
+	clock_t start = clock();
+	string my_answer = obj->fixCaps(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "Example four. The long fourth example. A. B. C. D.";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
